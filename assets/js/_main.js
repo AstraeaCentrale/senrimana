a130d889392f7afa5dc795f3dc7ef5408cf28c19
thebelovedmoon <103877195+thebelovedmoon@users.noreply.github.com>
2025-08-31 15:14:40 +0800

    some improvements to `?ifSpecial=true`

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -79,26 +79,55 @@ function ifSpecial() {
   let ifSpc = new URLSearchParams(window.location.search).get("ifSpecial");
   if (ifSpc == "true") {
     // instantiate things that needs instantiating
-    document.body.insertAdjacentHTML("afterbegin", `
-      <audio style="width: 0; height: 0;"></audio>
+    document.body.insertAdjacentHTML("beforeend", `
+      <div class="specialDialog"><div>
+        <audio mnPlyr style="width: 0; height: 0;"></audio>
+        <h2>special menu</h2>
+        <br>
+        <div>
+          <h3>mystery music</h3>
+          <div btnList>
+            <button senri onclick="{
+              document.querySelector('audio[mnPlyr]').src = 'https://raw.githubusercontent.com/AstraeaCentrale/senrimana/refs/heads/audio/SummerDreamRefrain_theme.mp3';
+              document.querySelector('audio[mnPlyr]').onended = () => {
+                document.querySelector('audio[mnPlyr]').play();
+                document.querySelector('audio[mnPlyr]').currentTime = 10.8125;
+              };
+              document.querySelector('audio[mnPlyr]').play();
+              document.querySelector('header').innerHTML = \`
+                <p class='flashPrompt' style='margin: 0; padding: 0.5rem; font-weight: bold;'>
+                  now playing: Summer Dream Refrain - Main Theme
+                </p>
+              \`;
+              document.querySelector('header').setAttribute('style', 'justify-content: center;');
+            }">play</button>
+          </div>
+        </div>
+        <br>
+        <div>
+          <h3>#StateOfVTuber Theme</h3>
+          <div btnList>
+            <button senri onclick="{
+              document.querySelector('audio[mnPlyr]').src = 'https://raw.githubusercontent.com/AstraeaCentrale/senrimana/refs/heads/audio/NewYearTwinkleCrisis_theme.mp3';
+              document.querySelector('audio[mnPlyr]').onended = () => { document.querySelector('audio[mnPlyr]').play(); };
+              document.querySelector('audio[mnPlyr]').play();
+              document.querySelector('header').innerHTML = \`
+                <p class='flashPrompt' style='margin: 0; padding: 0.5rem; font-weight: bold;'>
+                  now playing: New Year Twinkle Crisis! - Main Theme
+                </p>
+              \`;
+              document.querySelector('header').setAttribute('style', 'justify-content: center;');
+            }">play</button>
+            <button senri onclick="window.open('https://thebelovedmoon.github.io/?vt=stateofvtuber')">visit</button>
+          </div>
+        </div>
+      </div></div>
     `);
-    document.getElementById("btnSelSpecial").insertAdjacentHTML("beforeend", `
-      <span class="material-symbols-outlined"
-        title="listen to the mystery music.."
-        onclick="{
-          document.querySelector('audio').src = 'https://raw.githubusercontent.com/AstraeaCentrale/senrimana/refs/heads/audio/SummerDreamRefrain_theme.mp3';
-          document.querySelector('audio').onended = () => {
-            document.querySelector('audio').play();
-            document.querySelector('audio').currentTime = 10.8125;
-          };
-          document.querySelector('audio').play();
-          document.querySelector('header').innerHTML = \`
-            <p class='flashPrompt' style='margin: 0; padding: 0.5rem; font-weight: bold;'>
-              now playing: Summer Dream Refrain - Main Theme
-            </p>
-          \`;
-          document.querySelector('header').setAttribute('style', 'justify-content: center;');
-        }">music_note</span>
-    `);
+    document.querySelector(".specialDialog").setAttribute("style", `width: 100%; height: calc(100vh - ${getComputedStyle(document.querySelector("header")).height}); background-color: #e5e3e1; color: #3d374c; overflow-y: auto; scrollbar-width: thin;`);
+    // document.querySelector(".specialDialog *").setAttribute("style", "margin: 0 !important; display: block !important; padding: 0 !important;");
+    document.querySelector(".specialDialog > div").setAttribute("style", "width: 62.5rem; max-width: 100%; margin: 0 auto; text-align: center; padding: 3rem 1rem;");
+    document.querySelectorAll("div[btnList] *").forEach(btlst  => {
+      btlst.setAttribute("style", "display: inline-block; color: #e5e3e1;");
+    });
   }
 }
