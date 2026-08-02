8b12c791dfd8f5b7ef4ced4ae6aafa75d661a3b0
💚🍃Aga-chuu💗🌸 <103877195+thebelovedmoon@users.noreply.github.com>
2026-08-02 14:24:49 +0800

    千里真那2026 is complete🖤💜

--- assets/js/fetchFeed.js
+++ assets/js/fetchFeed.js
@@ -32,7 +32,7 @@ function broadcastLineup(year) {
       `;
     }
     document.getElementById("vodList").innerHTML = out;
-    isOngoing();
+    // isOngoing();
   }
   let ajxLoad = new XMLHttpRequest();
   ajxLoad.onreadystatechange = function() {
